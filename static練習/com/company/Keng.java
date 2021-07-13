733665c18d812eaf14598f69ca9b81c1fd413996
CHIN-HUA <tico9004040@gmail.com>
2021-07-13 09:37:18 +0800

    Create Keng.java

---
+++ static練習/com/company/Keng.java
@@ -0,0 +1,13 @@
+package com.company;
+
+
+public class Keng {
+    static  void printTwo()
+    {
+        System.out.println(2);
+    }
+    void printTwoDongGan()
+    {
+        System.out.println(3);
+    }
+}
